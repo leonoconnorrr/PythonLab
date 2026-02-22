a92a5820290664506ee0ab5faf675e6ac7b84a9e
leonoconnorrr <virgilorgil07@gmail.com>
2026-02-22 17:14:21 +0800

    fix: removed the unnecassary line of code

--- hero.py
+++ hero.py
@@ -8,9 +8,11 @@ class Battle:
     def action(self):
         print(f"{self.throw} throws {self.weapon} and {self.receive} catches it.")
 
-    def start(self, action):
-        action()
-
 battle1 = Battle("Thor", "Captain America", "Mjolnir")
 
-battle1.start(battle1.action)
+battle1.action()
+
+
+
+
+
